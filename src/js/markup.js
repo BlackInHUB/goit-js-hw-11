7f2b505f94c87c994519340c4593064390d06f4f
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-08-16 19:44:21 +0300

    80/20

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,33 +1,32 @@
-const gallery = document.querySelector('.gallery');
-
-function imgMarkup(imgArray) {
-  const markup = imgArray
-    .map(
-      img => `<div class="photo-card"><img src="${img.webformatURL}" alt="${img.tags}" loading="lazy"/>
-      <div class="info">
-    <p class="info-item">
-      <b>Likes:</b>&nbsp;${img.likes}
-    </p>
-    <p class="info-item">
-      <b>Views:</b>&nbsp;${img.views}
-    </p>
-    <p class="info-item">
-      <b>Comments:</b>&nbsp;${img.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads:</b>&nbsp;${img.downloads}
-    </p>
-  </div>
-</div>`
-    )
-    .join('');
-
-  gallery.insertAdjacentHTML('beforeend', markup);
-}
-
-function clearGallery() {
-  gallery.innerHTML = '';
+async function imgMarkup(imgArray) {
+  try {
+    const markup = await imgArray
+      .map(
+        img => `<div class="photo-card">
+        <a class="gallery__item" href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy"/>
+        </a>
+        <div class="info">
+        <p class="info-item">
+        <b>Likes</b>${img.likes}
+        </p>
+        <p class="info-item">
+        <b>Views</b>${img.views}
+        </p>
+        <p class="info-item">
+        <b>Comments</b>${img.comments}
+        </p>
+        <p class="info-item">
+        <b>Downloads</b>${img.downloads}
+        </p>
+        </div>
+        </div>`
+      )
+      .join('');
+    return markup;
+  } catch {
+    error => console.log(error);
+  }
 }
 
 export { imgMarkup };
-export { clearGallery };
--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,11 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ImgSearchApi from './getImages';
 import { imgMarkup } from './markup';
-import { clearGallery } from './markup';
 
 const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
 const imgSearchApi = new ImgSearchApi();
@@ -12,10 +13,10 @@ const imgSearchApi = new ImgSearchApi();
 form.addEventListener('submit', onSearchSubmit);
 loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
+loadMoreBtn.classList.add('is-hidden');
+
 function onSearchSubmit(evt) {
   evt.preventDefault();
-
-  loadMoreBtn.classList.add('is-hidden');
 
   imgSearchApi.query = evt.currentTarget.elements.searchQuery.value;
 
@@ -23,16 +24,11 @@ function onSearchSubmit(evt) {
   imgSearchApi
     .getImages()
     .then(images => {
-      if (images.data.totalHits === 0) {
-        return Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-
+      startHitsCheck(images);
       clearGallery();
       appendImgMarkup(images);
-      loadMoreBtn.classList.remove('is-hidden');
-      Notify.success(`Hooray! We found ${images.data.totalHits} images.`);
+      const lightbox = new SimpleLightbox('.gallery a');
+      hitsLeftCheck(images);
     })
     .catch(error => console.log(error));
 }
@@ -41,24 +37,48 @@ function onLoadMoreBtnClick() {
   imgSearchApi
     .getImages()
     .then(images => {
-      const hitsLeft =
-        images.data.totalHits - (imgSearchApi.page - 1) * imgSearchApi.per_page;
-
       appendImgMarkup(images);
-
-      if (hitsLeft <= 0) {
-        loadMoreBtn.classList.add('is-hidden');
-        return Notify.failure(
-          `We're sorry, but you've reached the end of search results.`
-        );
-      }
-
-      Notify.success(`Look! We have ${hitsLeft} more images.`);
+      const lightbox = new SimpleLightbox('.gallery a');
+      lightbox.refresh();
+      hitsLeftCheck(images);
     })
     .catch(error => console.log(error));
 }
 
+function startHitsCheck(images) {
+  if (images.data.totalHits === 0) {
+    imgSearchApi.resetPage();
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  Notify.success(`Hooray! We found ${images.data.totalHits} images.`);
+  loadMoreBtn.classList.remove('is-hidden');
+}
+
+function hitsLeftCheck(images) {
+  const totalHits = images.data.totalHits;
+  const hitsLeft = totalHits - (imgSearchApi.page - 1) * imgSearchApi.per_page;
+
+  if (hitsLeft < 0 && !loadMoreBtn.classList.contains('is-hidden')) {
+    loadMoreBtn.classList.add('is-hidden');
+    return Notify.failure(
+      `We're sorry, but you've reached the end of search results.`
+    );
+  } else if (hitsLeft > 0 && imgSearchApi.page > 2) {
+    Notify.success(`Look! We have ${hitsLeft} more images.`);
+  }
+}
+
 function appendImgMarkup(images) {
   const imgArray = images.data.hits;
-  imgMarkup(imgArray);
+  imgMarkup(imgArray)
+    .then(markup => {
+      gallery.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => console.log(error));
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
 }
